80e39d1ef9a221cee8797f8bec680ef5c83566cb
Connor Rigby <konnorrigby@gmail.com>
2019-12-02 09:34:00 -0800

    Day 1 part 2 complete

--- lib/day1.ex
+++ lib/day1.ex
@@ -3,6 +3,12 @@ defmodule AOC2019.Day1 do
   Fuel required to launch a given module is based on its mass. 
   Specifically, to find the fuel required for a module, 
   take its mass, divide by three, round down, and subtract 2.
+
+  So, for each module mass, calculate its fuel and 
+  add it to the total. Then, treat the fuel amount 
+  you just calculated as the input mass and repeat the 
+  process, continuing until a fuel requirement is zero 
+  or negative
   """
 
   @external_resource "input/day1.txt"
@@ -14,8 +20,14 @@ defmodule AOC2019.Day1 do
       |> String.split("\n")
       |> Enum.map(&String.to_integer/1)
 
-  def calculate_fuel(input_num) do
-    trunc(Float.floor(input_num / 3, 0) - 2)
+  def calculate_fuel(input_num, acc \\ []) do
+    case trunc(Float.floor(input_num / 3, 0) - 2) do
+      output_num when output_num <= 0 ->
+        Enum.reduce(acc, 0, fn output_num, acc -> output_num + acc end)
+
+      output_num ->
+        calculate_fuel(output_num, [output_num | acc])
+    end
   end
 
   def solve do
--- test/day1_test.exs
+++ test/day1_test.exs
@@ -3,9 +3,8 @@ defmodule AOC2019.Day1Test do
   alias AOC2019.Day1
 
   test "examples" do
-    assert Day1.calculate_fuel(12) == 2
     assert Day1.calculate_fuel(14) == 2
-    assert Day1.calculate_fuel(1969) == 654
-    assert Day1.calculate_fuel(100_756) == 33583
+    assert Day1.calculate_fuel(1969) == 966
+    assert Day1.calculate_fuel(100_756) == 50346
   end
 end
