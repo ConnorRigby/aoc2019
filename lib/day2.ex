f899a4e84ffa0a68c8705dbb7f4cd6623fdf0c12
Connor Rigby <konnorrigby@gmail.com>
2019-12-03 11:56:42 -0800

    Day 2 part 2 complete

--- lib/day2.ex
+++ lib/day2.ex
@@ -3,20 +3,59 @@ defmodule AOC2019.Day2 do
   To do this, before running the program, replace position 
   1 with the value 12 and replace position 2 with the value 2. 
   What value is left at position 0 after the program halts?
+
+  Find the input noun and verb that cause the program to produce the
+  output 19690720. What is 100 * noun + verb? (For example, if noun=12 and verb=2, 
+  the answer would be 1202.)
   """
 
   @external_resource "input/day2.txt"
   @input File.read!(@external_resource)
-  def input,
+  _ = File.mkdir_p(".cache/day2")
+
+  def input(noun, verb),
     do:
       @input
       |> String.trim()
       |> String.split(",")
       |> Enum.map(&String.to_integer/1)
+      |> list_replace(noun, 1)
+      |> list_replace(verb, 2)
 
-  def run(input \\ input(), pc \\ 0)
+  def solve(noun, verb, expected_answer) do
+    answer =
+      case File.read(".cache/day2/#{noun}.#{verb}") do
+        {:error, :enoent} ->
+          answer = run(noun, verb)
+          File.write(".cache/day2/#{noun}.#{verb}", to_string(answer))
+          answer
 
-  def run(input, pc) do
+        {:ok, answer_str} ->
+          answer_str |> String.trim() |> String.to_integer()
+      end
+
+    if answer == expected_answer do
+      100 * noun + verb
+    else
+      if noun == 99 do
+        solve(0, verb + 1, expected_answer)
+      else
+        solve(noun + 1, verb, expected_answer)
+      end
+    end
+  end
+
+  def run(noun, verb) do
+    [_, noun, verb | _] = initial_memory = input(noun, verb)
+    [result | _] = do_run(initial_memory)
+    IO.puts("noun=#{noun} verb=#{verb} result=#{result}")
+    # {(100 * noun) + verb, result}
+    result
+  end
+
+  def do_run(input, pc \\ 0)
+
+  def do_run(input, pc) do
     case :lists.nth(pc + 1, input) do
       99 ->
         input
@@ -25,13 +64,13 @@ defmodule AOC2019.Day2 do
         {_, [arg1, arg2, arg3 | _]} = :lists.split(pc + 1, input)
 
         exec(input, :add, {arg1, arg2, arg3})
-        |> run(pc + 4)
+        |> do_run(pc + 4)
 
       2 ->
         {_, [arg1, arg2, arg3 | _]} = :lists.split(pc + 1, input)
 
         exec(input, :mult, {arg1, arg2, arg3})
-        |> run(pc + 4)
+        |> do_run(pc + 4)
     end
   end
 
--- test/day2_test.exs
+++ test/day2_test.exs
@@ -3,9 +3,13 @@ defmodule AOC2019.Day2Test do
   alias AOC2019.Day2
 
   test "examples" do
-    assert Day2.run([1, 0, 0, 0, 99]) == [2, 0, 0, 0, 99]
-    assert Day2.run([2, 3, 0, 3, 99]) == [2, 3, 0, 6, 99]
-    assert Day2.run([2, 4, 4, 5, 99, 0]) == [2, 4, 4, 5, 99, 9801]
-    assert Day2.run([1, 1, 1, 4, 99, 5, 6, 0, 99]) == [30, 1, 1, 4, 2, 5, 6, 0, 99]
+    assert Day2.do_run([1, 0, 0, 0, 99]) == [2, 0, 0, 0, 99]
+    assert Day2.do_run([2, 3, 0, 3, 99]) == [2, 3, 0, 6, 99]
+    assert Day2.do_run([2, 4, 4, 5, 99, 0]) == [2, 4, 4, 5, 99, 9801]
+    assert Day2.do_run([1, 1, 1, 4, 99, 5, 6, 0, 99]) == [30, 1, 1, 4, 2, 5, 6, 0, 99]
+  end
+
+  test "part 2 examples" do
+    assert Day2.run(12, 2) == 3_706_713
   end
 end
